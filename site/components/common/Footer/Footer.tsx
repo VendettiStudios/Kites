186f54da6b5a0446f82872e05b115b0365392ef3
DH <daniel@vendettistudios.com>
2023-03-10 17:49:49 -0800

    carousel, footer

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -6,18 +6,35 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import s from './Footer.module.css'
 import Container from '@components/ui/Container/Container'
-
+import Image from 'next/image'
 interface Props {
     className?: string
     children?: any
 }
 
+
 const links = [
     {
-        name: 'Home',
-        url: '/',
+        name: 'Instagram',
+        url: 'https://www.instagram.com/kitesbycarlamanuel',
+    },
+    {
+        name: 'Location',
+        url: '/Location',
+    },
+    {
+        name: 'Contact',
+        url: '/Contact',
+    },
+    {
+        name: 'Policies',
+        url: '/Policies',
     },
 ]
+// Instagram
+// Location
+// Contact
+// Policies
 
 const Footer: FC<Props> = ({ className }) => {
     const rootClassName = cn(s.root, className)
@@ -26,46 +43,19 @@ const Footer: FC<Props> = ({ className }) => {
         <footer className={rootClassName}>
             <Container>
                 <div className={s.container}>
-                    <div className={s.container2}>
-                        <Link
-                            href="/"
-                            className={s.link1}
-                        >
-                            <span className={s.logo}>
-                            </span>
-                            <span>Kites by Carla Manuel</span>
-                        </Link>
-                    </div>
-                    <div className={s.container3}>
-                        <div className={s.container4}>
-                        </div>
-                    </div>
-                    <div className="col-span-1 lg:col-span-3 flex items-start lg:justify-end text-primary">
-                        <div className="flex space-x-4 items-center h-10">
-                            <a
-                                className={s.link}
-                                aria-label="Github Repository"
-                                href="https://github.com/vercel/commerce"
-                            >
-                            </a>
-                        </div>
-                    </div>
+                <div className={s.links} >
+            {links.map((link, index) => (
+              <Link key={index} href={link.url} style={{ textDecoration: 'none' }}>
+                <span className={s.link} >{link.name}</span>
+              </Link>
+            ))}
+          </div>
                 </div>
-                <div className="pt-6 pb-10 flex flex-col md:flex-row justify-between items-center space-y-4 text-accent-6 text-sm">
-                    <div>
-                        <span>&copy; 2023 Kites by Carla Manuel, Inc. All rights reserved.</span>
-                    </div>
-                    <div className="flex items-center text-primary text-sm">
-                        <span className="text-primary">Created by Daniel Holloway</span>
-                        <a
-                            rel="noopener noreferrer"
-                            href="https://vercel.com"
-                            aria-label="Vercel.com Link"
-                            target="_blank"
-                            className="text-primary"
-                        >
-                        </a>
-                    </div>
+                <div className={s.imageContainer}>
+                    <Image className={s.logo} src="/footerLogo.png" alt="nav Logo" width={75} height={100} />
+                </div>
+                <div className={s.footerText}>
+                    <span>&copy; 2023 Kites by Carla Manuel, Inc. All rights reserved.</span>
                 </div>
             </Container>
         </footer>
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,10 +1,16 @@
-import  Layout  from '@components/common/Layout/Layout'
+import Layout from '@components/common/Layout/Layout';
+import Carousel from '@components/ui/Carousel/Carousel';
 
+const Home = () => {
+  const images = ['/image1.jpg', '/image2.jpg', '/image3.jpg'];
 
-export default function Home() {
   return (
     <>
+      <Carousel images={images} />
     </>
-  )
-}
-Home.Layout = Layout
+  );
+};
+
+Home.Layout = Layout;
+
+export default Home;
